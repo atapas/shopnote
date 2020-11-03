5a710c1bfeffb1bea99fc5ee59b9140e21190200
Tapas Adhikary <tapas.adhikary@gmail.com>
2020-11-03 17:31:04 +0530

    Added a netlify toml file, a query js file and get shopnotes file to write a serverless function

---
+++ functions/get-shopnotes.js
@@ -0,0 +1,38 @@
+// get-shopnotes.js
+
+const query = require("./utils/query");
+
+ const GET_SHOPNOTES = `
+    query {
+        allShopNotes {
+        data {
+            _id
+            name
+            items {
+                data {
+                    name,
+                    checked,
+                    urgent,
+                    quantity
+                }
+            }
+        }
+    }
+    }  
+ `;
+
+  exports.handler = async () => {
+     const { data, errors } = await query(GET_SHOPNOTES);
+
+     if (errors) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify(errors)
+        };
+     }
+
+     return {
+       statusCode: 200,
+       body: JSON.stringify({ shopnotes: data.allShopNotes.data })
+     };
+   };
